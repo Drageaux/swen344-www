a87ae2f98f3f2b6906905b353055d8270ffe642d
David Thong Nguyen <thnguyen2808@gmail.com>
2018-02-01 12:53:53 -0500

    Can make seamless build scripts with package.json

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
 
-app.use('/', express.static(path.join(__dirname, 'client')));
+app.use('/', express.static(path.join(__dirname, 'dist')));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
