7671e25b883f82a17b08445b9ca77f9eb95b1cf1
David Thong Nguyen <thnguyen2808@gmail.com>
2018-02-01 16:48:36 -0500

    Added tabbed navigation

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {ErrorHandler, NgModule} from '@angular/core';
 import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
-import {SplashScreen} from '@ionic-native/splash-screen';
-import {StatusBar} from '@ionic-native/status-bar';
 
 import {MyApp} from './app.component';
-import {HomePage} from '../pages/home/home';
+import {HomePage} from "../pages/home/home";
+import {MagicPage} from "../pages/magic/magic";
+import {TabsPage} from "../pages/tabs/tabs";
+
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
 
 @NgModule({
     declarations: [
         MyApp,
-        HomePage
+        HomePage,
+        MagicPage,
+        TabsPage
     ],
     imports: [
         BrowserModule,
@@ -19,7 +24,9 @@ import {HomePage} from '../pages/home/home';
     bootstrap: [IonicApp],
     entryComponents: [
         MyApp,
-        HomePage
+        HomePage,
+        MagicPage,
+        TabsPage
     ],
     providers: [
         StatusBar,
